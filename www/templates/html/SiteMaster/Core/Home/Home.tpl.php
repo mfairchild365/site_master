5b5bf2ff3e5a033f36c52b00f27a4623247073e3
Michael Fairchild <mfairchild365@gmail.com>
2014-04-02 16:44:22 -0500

    Include more information about the service

--- www/templates/html/SiteMaster/Core/Home/Home.tpl.php
+++ www/templates/html/SiteMaster/Core/Home/Home.tpl.php
@@ -1,1 +1,20 @@
 Welcome!  Please log in to manage your sites or schedule scans.  You can use the registry as a WHOIS service.
+
+<h2>What is <?php echo \SiteMaster\Core\Config::get('SITE_TITLE') ?>?</h2>
+<p>
+    SiteMaster is a tool that is designed to help you.  It has two main functions:
+    <ul>
+        <li>Help with quality assurance</li>
+        <li>Help with communication</li>
+    </ul>
+</p>
+
+<h2>Quality Assurance</h2>
+<p>
+    <?php echo \SiteMaster\Core\Config::get('SITE_TITLE') ?> will scan websites for machine testable errors which can negatively affect the end user experience.  These errors are then reported to the people responsible for maintaining the website.
+</p>
+
+<h2>Communication</h2>
+<p>
+    <?php echo \SiteMaster\Core\Config::get('SITE_TITLE') ?> also provides a registry of websites, which you an use as a WHOIS service.  The registry allows people to find out who is responsible for websites.
+</p>
